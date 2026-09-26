501534ed8e8f2227142f21a7bbeea54947db7411
Vaishnavi-Patil20 <vaishnavi114a@gmail.com>
2026-09-26 19:51:05 +0530

    Delete ProtectedRoute.jsx

--- ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuthStore } from '../store/useStore';
-
-export const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuthStore();
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
